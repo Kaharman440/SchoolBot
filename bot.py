2105c7f3e2b959d4a8a7e2bd1eb702dd2551f2a8
Kaharman440 <kahashanhaiskiy@gmail.com>
2026-05-05 09:23:45 +0300

    bot.py
    Bot

--- bot.py
+++ bot.py
@@ -1,14 +1,16 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, CommandHandler, ContextTypes
 
-TOKEN = "8789731393:AAGTdFe9oPu_9vL4vC7X3M4qRY76cnM8pLg"
-ADMIN_ID = 5571386481  # твой Telegram ID
+# 🔐 Берём данные из Railway Variables
+TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 users = {}
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
-    
+
     users[user.id] = {
         "step": 1,
         "username": user.username,
@@ -17,11 +19,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(
         "Здравствуйте! Добро пожаловать на анонимный опрос.\n"
-        "Мы видим ответы, но не видим авторов *предназначено для вашей безопасности*. В каком вы классе?",
-        parse_mode="Markdown"
+        "В каком вы классе?"
     )
 
 async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message is None:
+        return
+
     user = update.effective_user
     user_id = user.id
     text = update.message.text
@@ -34,12 +38,13 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if step == 1:
         users[user_id]["name"] = text
         users[user_id]["step"] = 2
+
         await update.message.reply_text(
             "Что вы чувствуете приходя в школу?"
         )
 
     elif step == 2:
-        users[user_id]["class"] = text
+        users[user_id]["feelings"] = text
 
         username = users[user_id]["username"]
         first_name = users[user_id]["first_name"]
@@ -47,9 +52,9 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
         result = (
             f"Новый ответ:\n"
             f"Username: @{username if username else 'нет'}\n"
-            f"Имя в Telegram: {first_name}\n"
-            f"Ответ (класс): {users[user_id]['name']}\n"
-            f"Чувства: {users[user_id]['class']}"
+            f"Имя: {first_name}\n"
+            f"Класс: {users[user_id]['name']}\n"
+            f"Чувства: {users[user_id]['feelings']}"
         )
 
         await context.bot.send_message(chat_id=ADMIN_ID, text=result)
@@ -57,9 +62,11 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("Спасибо 👍")
         del users[user_id]
 
+# 🚀 запуск бота
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle))
 
-app.run_polling()
+if __name__ == "__main__":
+    app.run_polling()
